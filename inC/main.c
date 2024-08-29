f5d427941c0849e12e8e7bdc489189ac6e3c10ee
DNF <danfonseca3@hotmail.com>
2024-08-29 11:50:49 +0100

    Zoomed version working in C

--- inC/main.c
+++ inC/main.c
@@ -2,45 +2,54 @@
 #include "helper.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <unistd.h>  // Include this for sleep function
 
 #define MAX_ITER 650
 #define DENSE 200 
 
 int main(){ 
-    Axis* my_axis = create_axis(0.0, 0.0, 1.0, 1.0, DENSE);    if (my_axis == NULL) {
+    Axis* my_axis = create_axis(0.15, 0.0, 5.0, 2.0, DENSE);   
+     if (my_axis == NULL) {
         printf("Failed to create Axis object.\n");
         return 1;
     }
+    char display[DENSE][DENSE]; 
 
-   double* X = linspace(my_axis->center_x - my_axis->width_x/2, my_axis->center_x + my_axis->width_x/2, DENSE); 
-   double* Y = linspace(my_axis->center_y - my_axis->width_y/2, my_axis->center_y + my_axis->width_y/2, DENSE);
+    for(int i = 0; i < 100; i++){
 
-   char display[DENSE][DENSE]; 
-  for (int i = 0; i < DENSE; i++) {
-        for (int j = 0; j < DENSE; j++) {
-            int counter = iter(X[i], Y[j], MAX_ITER);
-            if (counter < MAX_ITER) {
-                display[i][j] = ' ';  // Use single quotes for characters
-            } else {
-                display[i][j]= '#';  // Use single quotes for characters
+        double* X = linspace(my_axis->center_x - my_axis->width_x/2, my_axis->center_x + my_axis->width_x/2, DENSE); 
+        double* Y = linspace(my_axis->center_y - my_axis->width_y/2, my_axis->center_y + my_axis->width_y/2, DENSE);
+
+        for (int i = 0; i < DENSE; i++) {
+            for (int j = 0; j < DENSE; j++) {
+                int counter = iter(X[i], Y[j], MAX_ITER);
+                if (counter < MAX_ITER) {
+                    display[i][j] = ' ';  // Use single quotes for characters
+                } else {
+                    display[i][j]= '#';  // Use single quotes for characters
+                }
             }
         }
-    }
-   
 
-   // Print the display array to the console
-    for (int i = 0; i < DENSE; i++) {
-        for (int j = 0; j < DENSE; j++) {
-            printf("%c", display[j][DENSE-1 - i]);  // Print each character
+        // ANSI escape code to clear the screen and move the cursor to the top left corner
+        printf("\033[2J\033[H");
+
+        // Print the display array to the console
+        for (int i = 0; i < DENSE; i++) {
+            for (int j = 0; j < DENSE; j++) {
+                printf("%c", display[j][DENSE-1 - i]);  // Print each character
+            }
+            printf("\n");  // Print a newline after each row
         }
-        printf("\n");  // Print a newline after each row
-    }
+        fflush(stdout);
 
+        update_axis(my_axis, 0.75, 0.75);
+        free(X);
+        free(Y);
+        usleep(1000000);
+    }
     // Free the allocated memory for the Axis object
     free(my_axis);
-    free(X);
-    free(Y);
 
     return 0;
 }
